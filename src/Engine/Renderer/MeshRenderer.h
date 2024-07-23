0a938628dd607a486fd4ee0e505a2a380a8fb4d4
liurirui <160838282+liurirui@users.noreply.github.com>
2024-07-23 18:51:38 +0800

    MeshRenderer

---
+++ src/Engine/Renderer/MeshRenderer.h
@@ -0,0 +1,65 @@
+#pragma once
+#include"Base/Shader.h"
+#include"Base/ShaderCode.h"
+#include"Base/Camera.h"
+#include<Base/Constants.h>
+#include <glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+#define MAX_BONE_INFLUENCE 4
+
+NAMESPACE_START
+
+struct Vertex {
+    // position
+    glm::vec3 Position;
+    // normal
+    glm::vec3 Normal;
+    // texCoords
+    glm::vec2 TexCoords;
+    // tangent
+    glm::vec3 Tangent;
+    // bitangent
+    glm::vec3 Bitangent;
+    //bone indexes which will influence this vertex
+    int m_BoneIDs[MAX_BONE_INFLUENCE];
+    //weights from each bone
+    float m_Weights[MAX_BONE_INFLUENCE];
+};
+
+
+struct Texture {
+    unsigned int id;
+    string type;
+    string path;
+};
+
+class Mesh {
+public:
+    // mesh Data
+    vector<Vertex>       vertices;
+    vector<unsigned int> indices;
+    vector<Texture>      textures;
+    unsigned int VAO;
+
+    // constructor
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+    void Draw(Shader& shader);
+
+
+private:
+    // render data 
+    unsigned int VBO, EBO;
+
+    // initializes all the buffer objects/arrays
+    void setupMesh();
+};
+
+NAMESPACE_END
